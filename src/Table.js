b4b4d8ac8a7d35326178d107e535162f745bdc96
Raza <rtshareef@gmail.com>
2021-04-24 17:46:17 -0400

    added deliverables to readme and can now filter events based on state

--- src/Table.js
+++ src/Table.js
@@ -1,23 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Table } from "reactstrap";
 
-export default class EventsTable extends Component {
-  render() {
-    console.log(this.props);
+// changing to function
+function EventsTable(props) {
+  const [classification, setClassification] = useState("GHSA");
 
-    const fhsaEvents = this.props.ghsa.map(event => {
-      console.log(event);
-      return (
-        <tr>
-          <td>{event.key}</td>
-          <td>{event.headline}</td>
-          <td>{event.subheadline}</td>
-          <td>{event.date}</td>
-        </tr>
-      );
-    });
+  console.log(props);
+  console.log(classification);
 
+  const changeClass = e => {
+    setClassification(e.target.value);
+  };
+
+  const txsaEvents = props.tsa.map(event => {
+    // console.log(event);
     return (
+      <tr>
+        <td>{event.key}</td>
+        <td>{event.headline}</td>
+        <td>{event.subheadline}</td>
+        <td>{event.date}</td>
+      </tr>
+    );
+  });
+
+  const ghsaEvents = props.ghsa.map(event => {
+    // console.log(event);
+    return (
+      <tr>
+        <td>{event.key}</td>
+        <td>{event.headline}</td>
+        <td>{event.subheadline}</td>
+        <td>{event.date}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      {" "}
+      <select id="ClassList" onChange={e => changeClass(e)}>
+        <option> Choose Classification</option>
+        <option value="FHSA"> GHSA </option>
+        <option value="TXSA"> TXSA </option>
+      </select>
       <div className="Events-Table">
         <Table dark>
           <thead>
@@ -28,9 +54,15 @@ export default class EventsTable extends Component {
               <th>Start time</th>
             </tr>
           </thead>
-          <tbody>{fhsaEvents}</tbody>
+          {classification !== "TXSA" ? (
+            <tbody>{ghsaEvents}</tbody>
+          ) : (
+            <tbody>{txsaEvents}</tbody>
+          )}
         </Table>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default EventsTable;
